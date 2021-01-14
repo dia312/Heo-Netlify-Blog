be5bec12b9552d1f5034b051a4b48460a0591936
HeoSeungYeon <dia0312@naver.com>
2021-01-14 15:27:44 +0900

    fix config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `HeoSeungYeon Blog`,
+  description: `Blog posted about Coding Study`,
+  author: `HeoSeungYeon`,
+  introduction: `To develop myself, To develop anything`,
+  siteUrl: `https://heoseungyeon.netlify.app/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://github.com/heoseungyeon`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
